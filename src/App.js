c13b669db433a8f74e8ba347c4c8dfecf0341612
Charlie <chumbert3124@icloud.com>
2024-08-17 23:51:57 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,1 @@
-import React from "react";
-import "./App.scss";
-import Main from "./containers/Main";
-
-function App() {
-  return (
-    <div>
-      <Main />
-    </div>
-  );
-}
-
-export default App;
+"Hello World!"
