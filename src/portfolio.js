aa84a92a41ae4e7ab5431474d2c24dfe5d24860c
Charlie <chumbert3124@icloud.com>
2024-08-18 20:31:21 -0700

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -287,6 +287,16 @@ const talkSection = {
   display: false // Set false to hide this section, defaults to true
 };
 
+const podcastSection = {
+  title: emoji("Podcast 🎙️"),
+  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: [
+    "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
 
 // Resume Section
 const resumeSection = {
@@ -330,6 +340,7 @@ export {
   bigProjects,
   achievementSection,
   blogSection,
+  podcastSection
   talkSection,
   contactInfo,
   twitterDetails,
