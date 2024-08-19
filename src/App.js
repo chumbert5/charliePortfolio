41e624ef1038b407a1053e9777745f79ca6684a2
Charlie <chumbert3124@icloud.com>
2024-08-18 21:50:40 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.scss";
 import Main from "./containers/Main";
+import bigProjects from "./components/bigProjects/bigProjects";
+import { bigProjects } from "./portfolio";
 
 function App() {
   return (
